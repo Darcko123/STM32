2d2fe3653f68b5c35bd5e21a5ac86ad752ab0641
Darcko123 <daniel18052002@yahoo.com>
2026-07-03 10:49:02 -0600

    docs(libs/ILI9341): release version v1.3.0
    - Migración a paleta de colores X11/CSS con 147 macros
    - Adición de primitivas de dibujo: líneas rápidas, elipses y arcos
    - Nuevas funciones para frame buffer (Fill, DrawCircle, DrawEllipse, DrawArc)
    - Inclusión de cabeceras math.h, float.h y stdbool.h"

--- Librerias/ILI9341_Disc1/ILI9341_Disc1.h
+++ Librerias/ILI9341_Disc1/ILI9341_Disc1.h
@@ -21,8 +21,8 @@
  *
  * @origin El código de este driver se basa en la librería Petr Machala, Tilen Majerle, 2014.
  * @author Dr. Luis Antonio Raygoza Pérez & Ing. Daniel Ruiz
- * @date June 15, 2026
- * @version 1.2.0
+ * @date July 03, 2026
+ * @version 1.3.0
  */
 
 #ifndef ILI9341_DISC1_H
